c80c96f6ae29d3809a6ae5f928257fef77164d13
sisjosex <sisjosex@gmail.com>
2015-04-28 16:21:41 -0400

    last cahgens

--- config.js
+++ config.js
@@ -1,37 +1,98 @@
 var offlineData =
     {
         "status": "success",
-        "pantallas": [{
-            "id": "1",
-            "imagen": "img\/pantalla\/img1.jpg",
-            "texto_facebook": "compartir x facebook",
-            "texto_twitter1": "compartir x twitter",
-            "texto_twitter2": "compartir x twitter2",
-            "texto_instagram": "compartir x instagram",
-            "texto_whatsapp": "compartir x w",
-            "status": "active",
-            "logo": ""
-        }, {
-            "id": "2",
-            "imagen": "img\/pantalla\/img2.jpg",
-            "texto_facebook": "facebook",
-            "texto_twitter1": "twitter",
-            "texto_twitter2": "t2",
-            "texto_instagram": "inst",
-            "texto_whatsapp": "compartir x w",
-            "status": "active",
-            "logo": ""
-        }, {
-            "id": "3",
-            "imagen": "img\/pantalla\/img1.jpg",
-            "texto_facebook": "fb",
-            "texto_twitter1": "tw1",
-            "texto_twitter2": "tw2",
-            "texto_instagram": "inst",
-            "texto_whatsapp": "compartir x w",
-            "status": "active",
-            "logo": ""
-        }],
+        "pantallas": [
+
+            {
+                "id": "1",
+                "imagen": "img/pantalla/img1.jpg",
+                "texto_facebook": "Descarga la App Like Cifuentes de GooglePlay o AppStore y consulta la propuesta de Cristina Cifuentes para las familias numerosas.",
+                "texto_twitter1": "",
+                "texto_twitter2": "Consulta el compromiso de @ccifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_instagram": "Consulta el compromiso de @cristinacifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_whatsapp": "Mira. Puedes consultar el compromiso de Cristina Cifuentes para las familias numerosas en la App \"Like Cifuentes\".",
+                "status": "active",
+                "logo": ""
+            },
+            {
+                "id": "2",
+                "imagen": "img/pantalla/img2.jpg",
+                "texto_facebook": "Descarga la App Like Cifuentes de GooglePlay o AppStore y consulta la propuesta de Cristina Cifuentes para las familias numerosas.",
+                "texto_twitter1": "",
+                "texto_twitter2": "Consulta el compromiso de @ccifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_instagram": "Consulta el compromiso de @cristinacifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_whatsapp": "Mira. Puedes consultar el compromiso de Cristina Cifuentes para las familias numerosas en la App \"Like Cifuentes\".",
+                "status": "active",
+                "logo": ""
+            },
+            {
+                "id": "3",
+                "imagen": "img/pantalla/img3.jpg",
+                "texto_facebook": "Descarga la App Like Cifuentes de GooglePlay o AppStore y consulta la propuesta de Cristina Cifuentes para las familias numerosas.",
+                "texto_twitter1": "",
+                "texto_twitter2": "Consulta el compromiso de @ccifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_instagram": "Consulta el compromiso de @cristinacifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_whatsapp": "Mira. Puedes consultar el compromiso de Cristina Cifuentes para las familias numerosas en la App \"Like Cifuentes\".",
+                "status": "active",
+                "logo": ""
+            },
+            {
+                "id": "4",
+                "imagen": "img/pantalla/img4.jpg",
+                "texto_facebook": "Descarga la App Like Cifuentes de GooglePlay o AppStore y consulta la propuesta de Cristina Cifuentes para las familias numerosas.",
+                "texto_twitter1": "",
+                "texto_twitter2": "Consulta el compromiso de @ccifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_instagram": "Consulta el compromiso de @cristinacifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_whatsapp": "Mira. Puedes consultar el compromiso de Cristina Cifuentes para las familias numerosas en la App \"Like Cifuentes\".",
+                "status": "active",
+                "logo": ""
+            },
+            {
+                "id": "5",
+                "imagen": "img/pantalla/img5.jpg",
+                "texto_facebook": "Descarga la App Like Cifuentes de GooglePlay o AppStore y consulta la propuesta de Cristina Cifuentes para las familias numerosas.",
+                "texto_twitter1": "",
+                "texto_twitter2": "Consulta el compromiso de @ccifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_instagram": "Consulta el compromiso de @cristinacifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_whatsapp": "Mira. Puedes consultar el compromiso de Cristina Cifuentes para las familias numerosas en la App \"Like Cifuentes\".",
+                "status": "active",
+                "logo": ""
+            },
+            {
+                "id": "6",
+                "imagen": "img/pantalla/img6.jpg",
+                "texto_facebook": "Descarga la App Like Cifuentes de GooglePlay o AppStore y consulta la propuesta de Cristina Cifuentes para las familias numerosas.",
+                "texto_twitter1": "",
+                "texto_twitter2": "Consulta el compromiso de @ccifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_instagram": "Consulta el compromiso de @cristinacifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_whatsapp": "Mira. Puedes consultar el compromiso de Cristina Cifuentes para las familias numerosas en la App \"Like Cifuentes\".",
+                "status": "active",
+                "logo": ""
+            },
+            {
+                "id": "7",
+                "imagen": "img/pantalla/img7.jpg",
+                "texto_facebook": "Descarga la App Like Cifuentes de GooglePlay o AppStore y consulta la propuesta de Cristina Cifuentes para las familias numerosas.",
+                "texto_twitter1": "",
+                "texto_twitter2": "Consulta el compromiso de @ccifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_instagram": "Consulta el compromiso de @cristinacifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_whatsapp": "Mira. Puedes consultar el compromiso de Cristina Cifuentes para las familias numerosas en la App \"Like Cifuentes\".",
+                "status": "active",
+                "logo": ""
+            },
+            {
+                "id": "8",
+                "imagen": "img/pantalla/img8.jpg",
+                "texto_facebook": "Descarga la App Like Cifuentes de GooglePlay o AppStore y consulta la propuesta de Cristina Cifuentes para las familias numerosas.",
+                "texto_twitter1": "",
+                "texto_twitter2": "Consulta el compromiso de @ccifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_instagram": "Consulta el compromiso de @cristinacifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+                "texto_whatsapp": "Mira. Puedes consultar el compromiso de Cristina Cifuentes para las familias numerosas en la App \"Like Cifuentes\".",
+                "status": "active",
+                "logo": ""
+            }
+
+        ],
         "config": {
             "id": "1",
             "contact_email": "joaquin.duro@miticatechnology.com",
@@ -39,20 +100,17 @@ var offlineData =
             "contact_body": "",
             "status": "active",
             "logo": "",
-            "titulo_final1": "Titulo Final1",
-            "titulo_final2": "Titulo Final2",
-            "titulo_final3": "Titulo Final3",
-            "texto_final1": "<p><strong>Texto Final1<\/strong><\/p>",
-            "texto_final2": "Titulo Final2",
-            "texto_final3": "Titulo Final3",
-            "imagen_final1": "img\/final.jpg",
-            "imagen_final2": "img\/final.jpg",
-            "imagen_final3": "img\/final.jpg",
-            "texto_facebook": "Texto compartir x facebook",
-            "texto_twitter1": "Texto compartir x twitter1",
-            "texto_twitter2": "Texto compartir x twitter2",
-            "texto_instagram": "Texto compartir x instagram",
-            "texto_whatsapp": "compartir x w",
+            "texto_final1": "En esas propuestas no hemos coincidido. Esperamos hacerlo con las próximas que te haremos en breve.",
+            "texto_final2": "¡Coincidimos en bastantes cosas! En breve tendremos nuevas propuestas para ti.",
+            "texto_final3": "¡Pensamos igual! En breve tendremos nuevas propuestas para ti.",
+            "imagen_final1": "menor25%.jpg",
+            "imagen_final2": "25%-70%.jpg",
+            "imagen_final3": "70%.jpg",
+            "texto_facebook": "Descarga la App Like Cifuentes de GooglePlay o AppStore y consulta la propuesta de Cristina Cifuentes para las familias numerosas.",
+            "texto_twitter1": "",
+            "texto_twitter2": "Consulta el compromiso de @ccifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+            "texto_instagram": "Consulta el compromiso de @cristinacifuentes con las familias numerosas en la App \"Like Cifuentes\".",
+            "texto_whatsapp": "Mira. Puedes consultar el compromiso de Cristina Cifuentes para las familias numerosas en la App \"Like Cifuentes\".",
             "web_texto": "www.cristinacifuentes.es",
             "web_url": "http://www.cristinacifuentes.es"
         }
